1f9959e9c885b1bea7bec02bef72a51e5dbe8101
jeoninhwa3 <jeoninhwa3@naver.com>
2022-05-12 17:15:54 +0900

    0512 update

--- js/app.js
+++ js/app.js
@@ -19,15 +19,31 @@ var swiper = new Swiper(".main-mySwiper", {
 
   // best-goods slide
   var swiper = new Swiper(".goods-mySwiper", {
-    cssMode: true,
-    loop:true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
     navigation: {
       nextEl: ".btn-next",
       prevEl: ".btn-prev",
     },
     pagination: {
       el: ".slide-pagination",
+      clickable: true,
     },
-    mousewheel: true,
-    keyboard: true,
+  });
+
+  // vod-intro
+  var swiper = new Swiper(".vod-mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 100,
+    loop: true,
+    freeMode: true,
+    navigation: {
+      nextEl: ".btn-next",
+      prevEl: ".btn-prev",
+    },
+    pagination: {
+      el: ".slide-pagination",
+      clickable: true,
+    },
   });
